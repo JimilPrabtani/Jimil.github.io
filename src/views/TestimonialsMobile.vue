71de278fc4606c727430ba6b9d8b1bf0916b664d
Jimil Prabtani <242184555+JimilPrabtani@users.noreply.github.com>
2026-02-14 13:15:23 +0530

    Refactor background image to use skyImage variable
    Updated background image implementation to use a variable.

--- src/views/TestimonialsMobile.vue
+++ src/views/TestimonialsMobile.vue
@@ -2,12 +2,13 @@
 import FramedMainSection from '@/layouts/FramedMainSection.vue';
 import testimonials from '@/data/testimonials.json';
 import TestimonialWriterCard from '@/components/TestimonialWriterCard.vue';
+import skyImage from '/backgrounds/sky.jpg';
 </script>
 
 <template>
     <FramedMainSection id="testimonials" class="min-h-[100dvh] flex relative">
         <div class="h-full w-full relative rounded-4xl text-white flex justify-center items-end text-center px-4 overflow-hidden">
-            <div class="absolute h-full w-full top-0 left-0 bg-[url('/backgrounds/sky.jpg')] bg-cover brightness-[.6] -z-10"></div>
+            <div class="absolute h-full w-full top-0 left-0 bg-cover brightness-[.6] -z-10" :style="{ backgroundImage: `url(${skyImage})` }"></div>
             <div class="flex h-full flex-col pt-[6dvw] md:flex-row">
 
                 <div v-for="(testimonial, i) in testimonials" :key="i"
