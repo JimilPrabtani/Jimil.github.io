e5e66bb384697bcd27b46c90b393bfee06aa6e22
Jimil Prabtani <242184555+JimilPrabtani@users.noreply.github.com>
2026-02-14 12:00:21 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // Using root path for custom domain (jimilpabtani.me) and user site (jimilprabtani.github.io)
+  base: '/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
